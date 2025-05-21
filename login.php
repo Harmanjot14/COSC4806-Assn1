137dcd5c7fe17d4e8af0c23eabd9502bd742ac79
Harmanjot14 <jotkharman@gmail.com>
2025-05-21 23:45:37 +0000

    Created login page with form- Non functional

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html
+<html>
+  <head>
+    <title>Login</title>
+  </head>
+
+  <body>
+
+    <h1>Login Forum</h1>
+    <form action="/validate.php" method="post">
+      <label for="fname">Username:</label>
+      <br>
+      <input type="text" id="username" name="username">
+      <br>
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password">
+      <br><br>
+      <input type="submit" value="Submit">
+      </form>
+    
+   
+  </body>
+
+</html>
