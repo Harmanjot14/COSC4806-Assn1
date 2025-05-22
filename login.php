b584af16abbaeac4d4d1677772888856808689af
Harmanjot14 <jotkharman@gmail.com>
2025-05-22 00:30:12 +0000

    Added <br> after password

--- login.php
+++ login.php
@@ -8,11 +8,12 @@
 
     <h1>Login Forum</h1>
     <form action="/validate.php" method="post">
-      <label for="fname">Username:</label>
+      <label for="username">Username:</label>
       <br>
       <input type="text" id="username" name="username">
       <br>
       <label for="password">Password:</label>
+      <br>
       <input type="password" id="password" name="password">
       <br><br>
       <input type="submit" value="Submit">
