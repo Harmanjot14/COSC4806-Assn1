c84a02e50c97170bf7c5482f22868190d5e40984
Harmanjot14 <jotkharman@gmail.com>
2025-05-22 00:29:49 +0000

    Added session start

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+?>
+
+
 <!DOCTYPE html
 <html>
   <head>
@@ -7,6 +12,7 @@
   <body>
     
     <h1>Assingment 1</h1>
+    <p>Welcome, <?=$_SESSION['username']?></p>
     <p><a href="/login.php">Click here to login</a></p>
     
   </body>
